b291c055dc6bdfdb90b927e4384e52762e9e797a
Danilo Mercado <mercadodanilo213@hotmail.com>
2025-01-24 01:11:09 -0300

    add toggleTheme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeContextPovider } from "./services/theme/theme.contex.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeContextPovider>
+      <App />
+    </ThemeContextPovider>
+  </React.StrictMode>
+);
