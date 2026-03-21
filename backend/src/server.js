363ce9d00656018f627257904d4119d7082303fe
danilomercado <mercadodanilo213@hotmail.com>
2026-03-21 16:04:05 -0300

    railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://TU-FRONT.vercel.app", // 🔥 cambiar después
+    ],
     credentials: true,
   }),
 );
